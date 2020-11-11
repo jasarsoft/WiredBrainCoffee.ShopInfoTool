fcabba19c61cb3a80f51d582c7b3e810e7ae98c0
Edin Jašarević <edinjasar14@gmail.com>
2020-11-11 23:49:21 +0100

    introduce command handlers

---
+++ WiredBrainCoffee.ShopInfoTool/HelpCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    internal class HelpCommandHandler : ICommandHandler
+    {
+        private readonly IEnumerable<CoffeeShop> _coffeeShops;
+
+        public HelpCommandHandler(IEnumerable<CoffeeShop> coffeeShops)
+        {
+            _coffeeShops = coffeeShops;
+            throw new NotImplementedException();
+        }
+
+        public void HandleCommand()
+        {
+            Console.WriteLine("> Available coffee shop commands:");
+            foreach (var coffeeShop in _coffeeShops)
+            {
+                Console.WriteLine($"> {coffeeShop.Location}");
+            }
+        }
+    }
+}
